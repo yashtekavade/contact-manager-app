ef1a461233212af5f034d1072fb0348a45d91671
bishh <yashtekavade@gmail.com>
2023-06-16 23:42:00 +0530

    accept and display

--- src/componets/App.js
+++ src/componets/App.js
@@ -1,7 +1,23 @@
+import React, { useState } from "react";
 import "./App.css";
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
 
 function App() {
-  return <div>Hello World</div>;
+  const [contacts, setContacts] = useState([]);
+
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts, contact]);
+  };
+  return (
+    <div className="ui container">
+      <Header />
+      <AddContact addContactHandler={addContactHandler} />
+      <ContactList contacts={contacts} />
+    </div>
+  );
 }
 
 export default App;
